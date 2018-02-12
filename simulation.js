6399ff94381040eb405e1a3565fc46e038dc7564
John Son <jsson77@gmail.com>
2018-02-11 19:17:06 -0800

    cleaned out clogs and new comments

--- simulation.js
+++ simulation.js
@@ -1,11 +1,9 @@
-//  NOTE: Run 10,000 times
-//  NOTE: results.jpg
-
+//  NOTE: Monty Hall Problem
 //  1. Generate the game set
 //  2. User picks one door
-//  3. Host picks (and shows) one door
+//  3. Host picks (and "shows") one door
 //  4. User either swithces or stays.
-//  5. Record result
+//  5. Return results
 
 function runMontyHall(games) {
   const totalGamesPlayed = games;
@@ -15,39 +13,33 @@ function runMontyHall(games) {
   while (games > 0) {
     //  1. Generate the game set
     const { doors, carInd } = generateGame();
-    console.log(games, 'doors: ', doors);
-    console.log(games, 'carInd: ', carInd);
 
     //  2. User picks one door
     const userChoice = userChooses();
-    console.log(games, 'userChoice: ', userChoice);
 
-    //  3. Host picks (and shows) one door
+    //  3. Host picks (and "shows") one door
     const hostChoice = hostChooses(doors, carInd, userChoice);
-    console.log(games, 'hostChoice: ', hostChoice);
 
     //  4. User either switches or stays.
-    //  User SWITCHES
-    const userSwitch = 3 - (hostChoice + userChoice);
-    console.log(games, 'userSwitch: ', userSwitch);
+    const userSwitchedChoice = userSwitches(hostChoice, userChoice);
 
-    if (userSwitch === carInd) switchWins++;
+    if (userSwitchedChoice === carInd) switchWins++;
     if (userChoice === carInd) stayWins++;
 
     games--;
   }
 
-  console.log('totalGamesPlayed: ', totalGamesPlayed);
-  console.log('switchWins: ', switchWins);
-  console.log('stayWins: ', stayWins);
+  console.log('runMontyHall totalGamesPlayed: ', totalGamesPlayed);
+  console.log('runMontyHall switchWins: ', switchWins);
+  console.log('runMontyHall stayWins: ', stayWins);
 
-  console.log('switch win percentage: ', switchWins/totalGamesPlayed * 100, '%');
-  console.log('stay win percentage: ', stayWins/totalGamesPlayed * 100, '%');
+  console.log(`switch win percentage: ${switchWins/totalGamesPlayed * 100}%`);
+  console.log(`stay win percentage: ${stayWins/totalGamesPlayed * 100}%`);
 }
 
-//  1. Generate the game set
+//  NOTE: generateGame() creates an Array 'doors', generates the door index that will be the car,
+//        fills the rest of the array with goats, and returns the doors[] along with car door/index
 function generateGame() {
-  //  random number from 1 - 3 ===> is the index of the Car.
   const doors = [];
   const carInd = Math.floor(Math.random() * 3);
   doors[carInd] = 1;
@@ -65,20 +57,15 @@ function generateGame() {
   }
 }
 
-//  2. User picks one door
+//  NOTE: userChooses() returns a random number that will be the index corresponding to the door the user chooses
 function userChooses() {
   return Math.floor(Math.random() * 3)
 }
 
-//  3. Host picks (and shows) one door
+//  NOTE: hostChooses() returns the index (door) that the host will choose, dependent on `usersChoice`
 function hostChooses(doors, carInd, userChoice) {
   if (userChoice === carInd) {
-    // the door is correct, host picks (randomly) one of the other two doors.
-    // console.log('Sanity:USER is CORRECT');
-    // console.log('doors: ', doors);
-    // console.log('carInd: ', carInd);
-    // console.log('userChoice: ', userChoice);
-
+    //  User chooses the correct door, host picks (randomly) one of the other two doors.
     let hostChoices = [];
     for (let i = 0; i < 3; i++) {
       if (i === userChoice) continue;
@@ -86,14 +73,7 @@ function hostChooses(doors, carInd, userChoice) {
     }
     return hostChoices[Math.floor(Math.random() * 2)];
   } else {
-    //  [1, 0, 0]
-    //      U  H
-    //  door is incorrect, therefore the other two has the car. Default to the other (unpicked) goat
-
-    // console.log('Sanity:USER is WRONG ', userChoice);
-    // console.log('doors: ', doors);
-    // console.log('carInd: ', carInd);
-
+    //  User chooses the incorrect door, therefore the other two has the car. Default to the other (unpicked) goat.
     for (let i = 0; i < 3; i++) {
       if (i === userChoice) continue;
       if (!doors[i]) return i;
@@ -101,4 +81,9 @@ function hostChooses(doors, carInd, userChoice) {
   }
 }
 
-runMontyHall(5);
+//  NOTE: User chooses a door which is not the user's original choice, `userChoice`, nor the host's `hostChoice`
+function userSwitches(hostChoice, userChoice) {
+  return 3 - (hostChoice + userChoice);
+}
+
+runMontyHall(10000);
