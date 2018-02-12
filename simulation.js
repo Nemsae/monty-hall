20c07b0ed5980ed5d27fdaf34fdc8e6ca9039f03
John Son <jsson77@gmail.com>
2018-02-11 18:20:38 -0800

    pseudocode

---
+++ simulation.js
@@ -0,0 +1,8 @@
+//  NOTE: Run 10,000 times
+//  NOTE: results.jpg
+
+//  1. Generate the game set
+//  2. User picks one door
+//  3. Host picks (and shows) one door
+//  4. User either swithces or stays.
+//  5. Record result
