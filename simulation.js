ed6d08f26c3359f20becc4f2e599611272d9c77f
John Son <jsson77@gmail.com>
2018-02-11 18:49:13 -0800

    refactor out user choice fxn and host choice fxn

--- simulation.js
+++ simulation.js
@@ -7,50 +7,24 @@
 //  4. User either swithces or stays.
 //  5. Record result
 
-let games = 10;
+let games = 5;
 
 while (games > 0) {
+  //  1. Generate the game set
   const { doors, carInd } = generateGame();
-  // console.log('doors: ', doors);
-  // console.log('carInd: ', carInd);
+  console.log(games, 'doors: ', doors);
+  console.log(games, 'carInd: ', carInd);
 
   //  2. User picks one door
-  //  Contestant picks random number (index)
-  const userChoice = Math.floor(Math.random() * 3);
-  //  [1, 0, 0]
-  //      U
+  const userChoice = userChooses();
+  console.log(games, 'userChoice: ', userChoice);
 
   //  3. Host picks (and shows) one door
-  const hostChoice = ;
-  //  did contestant pick the car? if so that leaves the other two as goats.
-  if (userChoice === carInd) {
-    // the door is correct, host picks (randomly) one of the other two doors.
-    // console.log('Sanity:USER is CORRECT');
-    // console.log('doors: ', doors);
-    // console.log('carInd: ', carInd);
-    // console.log('userChoice: ', userChoice);
+  const hostChoice = hostChooses(doors, carInd, userChoice);
+  console.log(games, 'hostChoice: ', hostChoice);
 
-    let hostChoices = [];
-    for (let i = 0; i < 3; i++) {
-      if (i === userChoice) continue;
-      hostChoices.push(i);
-    }
-    hostChoice = hostChoices[Math.floor(Math.random() * 2)];
-  } else {
-    //  [1, 0, 0]
-    //      U  H
-    //  door is incorrect, therefore the other two has the car. Default to the other (unpicked) goat
+  //  4. User either swithces or stays.
 
-    // console.log('Sanity:USER is WRONG ', userChoice);
-    // console.log('doors: ', doors);
-    // console.log('carInd: ', carInd);
-
-    for (let i = 0; i < 3; i++) {
-      if (i === userChoice) continue;
-      if (!doors[i]) hostChoice = i;
-    }
-  }
-  // console.log('hostChoice: ', hostChoice);
 
   games--;
 }
@@ -75,6 +49,38 @@ function generateGame() {
   }
 }
 
-function hostChooses() {
+//  2. User picks one door
+function userChooses() {
+  return Math.floor(Math.random() * 3)
+}
 
+//  3. Host picks (and shows) one door
+function hostChooses(doors, carInd, userChoice) {
+  if (userChoice === carInd) {
+    // the door is correct, host picks (randomly) one of the other two doors.
+    // console.log('Sanity:USER is CORRECT');
+    // console.log('doors: ', doors);
+    // console.log('carInd: ', carInd);
+    // console.log('userChoice: ', userChoice);
+
+    let hostChoices = [];
+    for (let i = 0; i < 3; i++) {
+      if (i === userChoice) continue;
+      hostChoices.push(i);
+    }
+    return hostChoices[Math.floor(Math.random() * 2)];
+  } else {
+    //  [1, 0, 0]
+    //      U  H
+    //  door is incorrect, therefore the other two has the car. Default to the other (unpicked) goat
+
+    // console.log('Sanity:USER is WRONG ', userChoice);
+    // console.log('doors: ', doors);
+    // console.log('carInd: ', carInd);
+
+    for (let i = 0; i < 3; i++) {
+      if (i === userChoice) continue;
+      if (!doors[i]) return i;
+    }
+  }
 }
